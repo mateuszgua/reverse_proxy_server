b612a5043690f1c0dfb072f7cf919b90159cba74
Mateusz Gula <mateuszgua@wp.eu>
2022-12-16 16:55:13 +0100

    Feat: add reverse proxy server

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ func main() {
 		fmt.Printf("[origin server] received request at: %s\n", time.Now())
 		_, _ = fmt.Print(r, "origin server response")
 	})
+	fmt.Print(originServerHandler)
+
+	reverseProxy := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("[reverse proxy server] received request at %s\n", time.Now())
+	})
+
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
 		httpPort = "8090"
@@ -21,5 +27,5 @@ func main() {
 	port := fmt.Sprintf(":%s", httpPort)
 	log.Printf("Starting server on http://localhost%s", port)
 
-	log.Fatal(http.ListenAndServe(port, originServerHandler))
+	log.Fatal(http.ListenAndServe(port, reverseProxy))
 }
